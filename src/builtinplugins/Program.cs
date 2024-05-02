9b5d3bc335d19af49fc36b958d3040920f6fe5a5
romeelk <romeel_k@yahoo.co.uk>
2024-05-02 17:16:47 +0100

    Added prompt examples

---
+++ src/builtinplugins/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+static (string? azoaEndpoint, string? azoaApiKey, string? aoaiModelId, string? azoaDeployedModel) GetConfig(IConfiguration config)
+{
+    var azoaEndpoint = config["azoaEndpoint"];
+    var azoaApikey = config["azoaApiKey"];
+    var azoaModelId = config["azoaModelId"];
+    var azoaDeployedModel = config["azoaDeployedModel"];
+    return (azoaEndpoint, azoaApikey, azoaModelId, azoaDeployedModel);
+}
+var (azoaEndpoint, azoaApikey, azoaModel, azoaDeployedModel) = GetConfig(configuration);
+
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName: azoaDeployedModel,
+    endpoint: azoaEndpoint,
+    apiKey: azoaApikey,
+    modelId: azoaModel);
+
+// Load some in built plugins
+builder.Plugins.AddFromType<TimePlugin>();
+builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+builder.Plugins.AddFromType<FileIOPlugin>();
+
+var kernel = builder.Build();
+
+await GetDateTimeInfo(kernel);
+await GetConversationTopics(kernel);
+
+var pluginFileToRead = Path.Combine(Environment.CurrentDirectory,"File.txt");
+
+await GetFileAndPassToPrompt(kernel, pluginFileToRead);
+// Invoke Time Plugin
+static async Task GetDateTimeInfo(Kernel kernel)
+{
+    Console.WriteLine("SK TimePlugin --> Tell me the current Day of week");
+    var currentDay = await kernel.InvokeAsync(nameof(TimePlugin), "DayOfWeek");
+    Console.WriteLine(currentDay);
+
+    Console.WriteLine("SK TimePlugin --> Tell me the current time");
+    var currentTime = await kernel.InvokeAsync(nameof(TimePlugin), "Time");
+    Console.WriteLine(currentTime);
+}
+static async Task GetConversationTopics(Kernel kernel)
+{
+    string input = @"I am a graduate seeking to learn Python. Provide me a list of learning materials which will assist me";
+
+    Console.WriteLine(input);
+    var result = await kernel.InvokeAsync(
+    "ConversationSummaryPlugin",
+    "GetConversationTopics",
+    new() { { "input", input } });
+
+    Console.WriteLine(result);
+}
+
+
+static async Task GetFileAndPassToPrompt(Kernel kernel, string filename)
+{
+    Console.WriteLine("---> Using FilePlugin to read a prompt from a file");
+
+    var fileContents = await kernel.InvokeAsync(nameof(FileIOPlugin), "Read",  new() { { "path", filename } });
+
+    Console.WriteLine($"File contents: {fileContents}");
+
+    var result  = await kernel.InvokePromptAsync(fileContents.ToString());
+    Console.WriteLine($"Result; {result}");
+}
---
+++ src/promptexamples/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.Core;
+
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+static (string? azoaEndpoint, string? azoaApiKey, string? aoaiModelId, string ? azoaDeployedModel) GetConfig(IConfiguration config)
+{
+    var azoaEndpoint = config["azoaEndpoint"];
+    var azoaApikey = config["azoaApiKey"];
+    var azoaModelId = config["azoaModelId"];
+    var azoaDeployedModel = config["azoaDeployedModel"];
+    return (azoaEndpoint, azoaApikey, azoaModelId, azoaDeployedModel);
+}
+var (azoaEndpoint, azoaApikey, azoaModel, azoaDeployedModel) = GetConfig(configuration);
+
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    deploymentName: azoaDeployedModel,
+    endpoint:azoaEndpoint,
+    apiKey:azoaApikey,
+    modelId:azoaModel);
+
+builder.Plugins.AddFromType<TimePlugin>();
+builder.Plugins.AddFromType<ConversationSummaryPlugin>();
+
+var kernel = builder.Build();
+
+
+await PromptWithHistory(kernel);
+await PromptTravelPersona(kernel);
+await PromptSystemMessage(kernel);
+
+static async Task PromptWithHistory(Kernel kernel)
+{
+    string language = "Python";
+    string history = @"I am keen programming student wanting to know more about python";
+    // Assign a persona to the prompt
+    string prompt = @$"
+    You are AI Python programming assistant. You are helpful, creative, 
+    and very friendly. Consider the prograammers's background:
+    ${history}
+
+    Create a list of helpful phrases and words in 
+    ${language} a programmer would find useful.
+
+    Group phrases by category. Include common Python 
+    statementss. Display the statements in the following format: 
+    Python:
+
+    Begin with: 'Here are some statments in ${language} 
+    you may find helpful:' 
+    and end with: 'I hope this helps you Python Learning'";
+    
+    Console.WriteLine($"Prompt history: {history}");
+    Console.WriteLine($"Prompt:{prompt}");
+    var result = await kernel.InvokePromptAsync(prompt);
+    Console.WriteLine($"Prompt response: {result}");
+}
+
+static async Task PromptTravelPersona(Kernel kernel)
+{
+    string input = @"I'm planning an anniversary trip with my 
+    spouse. We like hiking, mountains, and beaches. Our 
+    travel budget is $15000";
+
+    string prompt = @$"
+    The following is a conversation with an AI travel 
+    assistant. The assistant is helpful, creative, and 
+    very friendly.
+
+    <message role=""user"">Can you give me some travel 
+    destination suggestions?</message>
+
+    <message role=""assistant"">Of course! Do you have a 
+    budget or any specific activities in mind?</message>
+
+    <message role=""user"">${input}</message>";
+
+    Console.WriteLine($"Prompt :{prompt}");
+    var result = await kernel.InvokePromptAsync(prompt);
+    Console.WriteLine($"Prompt response with Travel Persona Role: {result}");
+}
+
+static async Task PromptSystemMessage(Kernel kernel)
+{
+    var input = @$"I want to learn the Javascript language. Please summarise 
+    for me a learning path";
+
+    string prompt = @$"
+    <message role=""system"">The following is a conversation with an AI Programming 
+    assistant. The assistant is helpful
+    very friendly.</message>
+
+    <message role=""user"">${input}</message>";
+
+    Console.WriteLine($"Prompt {prompt}");
+    var result = await kernel.InvokePromptAsync(prompt);
+    Console.WriteLine($"Prompot response: {result}");
+}
